66e559b29ed5fb8ed662ed5904e807f99ef06b1c
Amos Yeo <amosyeo.sh@gmail.com>
2024-12-19 23:51:52 +0800

    DOM Calc function done.

--- app.js
+++ app.js
@@ -1,92 +1,97 @@
 /*-------------------------------- Constants --------------------------------*/
 let proxyAns = 0;
-
+let proxyNum1 = 0;
+let proxyNum2 = 0;
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 const buttons = document.querySelectorAll('.button');                   //If more than one, use the All. Else it will select the first or the last one?
 const calculator = document.querySelector('#calculator');               //If only one, just querySelector will do.
-const operator = document.querySelector('.operator');
-const equal = document.querySelector('.equals');
-const num = document.querySelectorAll(`.number`);
+// const operator = document.querySelector('.operator');
+// const equal = document.querySelector('.equals');
+// const num = document.querySelectorAll(`.number`);
 /*----------------------------- Event Listeners -----------------------------*/
-// buttons.forEach((button) => {
-//   button.addEventListener('click', (event) => {
-//     // This log is for testing purposes to verify we're getting the correct value
-//     console.log(event.target.innerText);
-//     // Future logic to capture the button's value would go here...
-//   });
-// });
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    proxyNum1 = event.target.innerText;
+    console.log(proxyNum1);
+    // console.log(event.target.innerText);
+   
+  });
+});
 
-// calculator.addEventListener('click', (event) => {
-//   // This log is for testing purposes to verify we're getting the correct value
-//   // You have to click a button to see this log
-//   console.log(event.target.innerText);
 
-//   // Example
-//   if (event.target.classList.contains('number')) {
-//     // Do something with a number
-//   }
-
-//   // Example
-//   if (event.target.innerText === '*') {
-//     // Do something with this operator
-//   }
-// });
-
-num.addEventListener(`click`, (event) => {
-
-})
 
 /*-------------------------------- Functions --------------------------------*/
 
 
+// 1. Get numbers (Extract numbers to console)
+// 2. Store numbers into storage like num1 and num2 (Collate 2 digits numeber possible???)(while loop possible till operator pressed for break?)
+// 2a. Display numbers in the //!class Display (Find a way to insert the value to the Display, 2 digit possible?)
+// 3. Create function for calculation with 2 parameters (DONE!!!)
+// 4. Return answer with //! class Equal
+// 5. Clear and reset to initial state.
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
 
 
 
 
-const calculate = (operator,num1,num2) => {
-    switch (operator){
-        case `+`:
-            return addNum(num1,num2);
-            break;
-        case `-`:
-            return subtractNum(num1,num2);
-            break;
-        case `*`:
-            return multiplyNum(num1,num2);
-            break;
-        case `/`:
-            return divideNum(num1,num2);
-            break;
 
-    }
-}
+// const calculate = (operator,num1,num2) => {          //!works with the below function.
+//     switch (operator){
+//         case `+`:
+//             return addNum(num1,num2);
+//             break;
+//         case `-`:
+//             return subtractNum(num1,num2);
+//             break;
+//         case `*`:
+//             return multiplyNum(num1,num2);
+//             break;
+//         case `/`:
+//             return divideNum(num1,num2);
+//             break;
+
+//     }
+// }
 
 
 
-const addNum = (num1,num2) => {
-    proxyAns = num1+num2;
-    console.log(proxyAns);
-    return proxyAns;
-}
-const subtractNum = (num1,num2) => {
-    proxyAns = num1-num2;
-        console.log(proxyAns);
-    return proxyAns;
-}
-const multiplyNum = (num1,num2) => {
-    proxyAns = num1*num2;
-        console.log(proxyAns);
-    return proxyAns;  
-}
-const divideNum = (num1,num2) => {
-    proxyAns = num1/num2;
-        console.log(proxyAns);
-    return proxyAns;  
-}
+// const addNum = (num1,num2) => {
+//     proxyAns = num1+num2;
+//     console.log(proxyAns);
+//     return proxyAns;
+// }
+// const subtractNum = (num1,num2) => {
+//     proxyAns = num1-num2;
+//         console.log(proxyAns);
+//     return proxyAns;
+// }
+// const multiplyNum = (num1,num2) => {
+//     proxyAns = num1*num2;
+//         console.log(proxyAns);
+//     return proxyAns;  
+// }
+// const divideNum = (num1,num2) => {
+//     proxyAns = num1/num2;
+//         console.log(proxyAns);
+//     return proxyAns;  
+// }
 
-// addNum(1,5);
-// subtractNum(1,5);
-// multiplyNum(1,5);
-// divideNum(1,5);
+// // addNum(1,5);
+// // subtractNum(1,5);
+// // multiplyNum(1,5);
+// // divideNum(1,5);
+
+// calculate(`+`,1,5); //!works!
